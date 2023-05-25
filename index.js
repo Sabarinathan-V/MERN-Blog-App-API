b928589b9aeff461141bd79e24cad7bb5f8d7d36
sabarinathan <sabarivanam@gmail.com>
2023-05-25 16:08:15 +0530

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-app.use(cors({ credentials: true, origin: "https://myblog-mern-fullstack-sabarinathan-v.netlify.app/"}));
+app.use(cors({ credentials: true }));
 app.use(express.json());
 
 
