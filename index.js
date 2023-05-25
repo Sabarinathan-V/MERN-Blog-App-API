54a5befc6d08c373197f558e71ae339336d1d97c
sabarinathan <sabarivanam@gmail.com>
2023-05-25 14:06:54 +0530

    frontend url update

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const path = require("path");
 require("dotenv").config();
 
 app.use(express.json());
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: process.env.REACT_FRONTEND_URL }));
 app.use(cookieParser());
 
 // salt and secret key
